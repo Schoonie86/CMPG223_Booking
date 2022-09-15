c4854b276213b48c66803da16d2d22c83c99e2cb
Johan Schoonen <JOHAN_S@live.co.za>
2022-09-15 20:39:55 +0200

    Johan_15_9Update

---
+++ CMPG223_Booking/Classes/UserClass.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMPG223_Booking.Classes
+{
+   
+    public class UserClass
+    { 
+        public string name;
+        public string surname;
+        public string password;
+        public string email;
+        public int type; // 0=Admin 1=Traner 2=Client
+        
+        public void createUser(string name, string surname, string password, string email, int type)
+        {
+            try
+            {
+                
+            }
+            catch
+            {
+
+            }
+        }
+        public void updateUser()
+        {
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+        }
+        public void deleteUser()
+        {
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+        }
+        public void createClient()
+        {
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+        }
+        public void updateClient()
+        {
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+        }
+        public void deleteClient()
+        {
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+        }
+
+    }
+}
